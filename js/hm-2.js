c1939830cbe2687ba51de3d85d7e264f45cb451b
Dima <96godin@gmail.com>
2025-06-20 22:50:07 +0300

    hm-2

--- js/hm-2.js
+++ js/hm-2.js
@@ -7,7 +7,7 @@ function getRandomInt(min, max){
     return Math.floor(Math.random() * (max - min) + min)
 }
 
-console.log(getRandomInt(1, 10));
+console.log(getRandomInt(1, 10));  
 console.log(getRandomInt(1, 100));
 console.log(getRandomInt(40, 50));
 
@@ -19,7 +19,7 @@ function greet(msg, name) {
     return `${msg} ${name}`
 }
 
-console.log(greet("Hi", "Bob"))
+console.log(greet("Hi", "Bob"))  // Hi Bob
 
 
 
@@ -29,4 +29,4 @@ function sumBigIntegers(numStr1, numStr2){
     return BigInt(numStr1) + BigInt(numStr2);
 }
 
-console.log(sumBigIntegers(9007199254740991, 9007199254740991))
+console.log(sumBigIntegers(9007199254740991, 9007199254740991)) // 18014398509481982n
